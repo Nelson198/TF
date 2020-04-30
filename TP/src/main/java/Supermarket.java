776502c17d46dde6987bc8e12ce70ba3b4e61085
Nelson198 <nteixeira88@gmail.com>
2020-04-30 15:56:55 +0100

    #7 Commit: Sugestão para a interface do supermercado

--- TP/src/main/java/Supermarket.java
+++ TP/src/main/java/Supermarket.java
@@ -2,5 +2,14 @@
  * Supermarket Interface
  */
 public interface Supermarket {
-    
+    // Iniciar compra / Criar carrinho
+    void start();
+    // Consultar o preço de um produto
+    float getPrice(int id);
+    // Verificar a disponibilidade de um produto
+    boolean isAvailable(int id);
+    // Acrescentar um ou mais produtos ao carrinho de compras
+    void add(int id, int amount);
+    // Confirmar encomenda, indicando se foi concretizada com sucesso
+    boolean checkout(int id);
 }
