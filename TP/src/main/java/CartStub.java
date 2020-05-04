1aca53b62f88e3675f16abdf7a147d3980b07243
SacitobJose <zezinhoboticas@gmail.com>
2020-05-04 11:44:29 +0100

    client, stub and message class

--- TP/src/main/java/CartStub.java
+++ TP/src/main/java/CartStub.java
@@ -14,28 +14,45 @@ import java.util.concurrent.Executors;
  * Cart Stub
  */
 public class CartStub {
+    String idCart;
+
     Serializer serializer = new SerializerBuilder().addType(String.class).build();
+
     ManagedMessagingService ms;
     Address serverAddress;
-    ExecutorService executor = Executors.newFixedThreadPool(1);
-    CompletableFuture<CartImpl> res;
 
-    public CartStub(Address myAddress, Address serverAddress) throws ExecutionException, InterruptedException {
+    ExecutorService executor = Executors.newFixedThreadPool(1);
+    CompletableFuture<Boolean> res;
+
+    public CartStub(ManagedMessagingService ms, Address serverAddress) throws ExecutionException, InterruptedException {
+        // Initialize the messaging service
+        this.ms = ms;
         this.serverAddress = serverAddress;
 
-        // Initialize the messaging service
-        this.ms = new NettyMessagingService("cart", myAddress, new MessagingConfig());
-
         this.ms.registerHandler("res", (a, b) -> {
-            CartImpl res = serializer.decode(b);
+            boolean res = serializer.decode(b);
             this.res.complete(res);
+        }, this.executor);
+
+        this.ms.registerHandler("newCart", (a, b) -> {
+            idCart = serializer.decode(b);
         }, this.executor);
 
         ms.start().get();
     }
 
-    public void addProduct(String idProduct) throws ExecutionException, InterruptedException {
+    public void addProduct(String idProduct, int qtd) {
+        CartUpdate toSend = new CartUpdate(idCart, idProduct, qtd);
+        this.ms.sendAsync(serverAddress, "addProduct", this.serializer.encode(toSend));
+    }
+
+    public void removeProduct(String idProduct, int qtd) {
+        CartUpdate toSend = new CartUpdate(idCart, idProduct, qtd);
+        this.ms.sendAsync(serverAddress, "removeProduct", this.serializer.encode(toSend));
+    }
+
+    public void checkout() {
         this.res = new CompletableFuture<>();
-        this.ms.sendAsync(serverAddress, "addProduct", this.serializer.encode(idProduct));
+        this.ms.sendAsync(serverAddress, "checkout", this.serializer.encode(idCart));
     }
 }
