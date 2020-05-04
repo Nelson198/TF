895ba48461831df3164ce645e0905d298471ef52
SacitobJose <zezinhoboticas@gmail.com>
2020-05-04 09:50:19 +0100

    classe produto concluída

--- TP/src/main/java/Product.java
+++ TP/src/main/java/Product.java
@@ -2,7 +2,42 @@
  * Product
  */
 public class Product {
-    public static void main(String[] args) {
-        
+    String id;
+    String name;
+    String description;
+    double price;
+
+    public Product(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Product(Product p) {
+        this.id = p.getId();
+        this.name = p.getName();
+        this.description = p.getDescription();
+        this.price = p.getPrice();
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double newPrice) {
+        this.price = newPrice;
     }
 }
